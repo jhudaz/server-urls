744b9a1abead3d61a14bb97436391e0c1aee38cf
Jaime Velez <jandres.vr@hotmail.com>
2019-02-03 12:03:07 -0500

    setting the data base connection

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const Sequelize = require('sequelize');
 const app = express()
-const port = procces.env.PORT || 4000
+const port = process.env.PORT || 4000
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOSTNAME,
+  dialect: process.env.DIALECT,
+});
+
 
 app.use(cors())
-app.unsubscribe(bodyParser())
+app.use(bodyParser())
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
